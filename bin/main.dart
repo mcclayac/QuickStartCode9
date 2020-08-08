679d6bec027466af0fb4a1664f016e9e00dcebc8
mcclayac <anthony.mcclay@me.com>
2020-08-08 13:07:01 -0400

    Added libraries

--- bin/main.dart
+++ bin/main.dart
@@ -1,3 +1,15 @@
+
+import 'dart:math' as math;
+import 'package:QuickStartCode9/PowProject.dart';
+import 'package:QuickStartCode9/RelationalOperators.dart' as relation;
+
+
+//import 'package:PowProject.dart'
+//import 'package:IdeaProjects/PowProject.dart';
+//import 'package:IdeaProjects/RelationalOperators.dart' as relation;
+
+
+
 void main(List<String> arguments) {
   print("----------------------------------------------");
   print("----------------------------------------------");
@@ -10,5 +22,15 @@ void main(List<String> arguments) {
   // https://learning.oreilly.com/library/view/quick-start-guide/9781484255629/A490744_1_En_9_Chapter.html
 
 
-  
+  print("Printing 2 to the power 5 using Dart's built-in 'dart:math' library.");
+  var int = math.pow(2, 5);
+  print(int);
+  print("Now we are going to use another 'pow()' function from our own library.");
+  var anotherPowObject = PowProject();
+  anotherPowObject.MultiplyByAGivenNumber(4, 3);
+  anotherPowObject.pow(2, 12);
+  print("Now we are going to use another library to test the relational operators.");
+  var trueOrFalse = relation.TrueOrFalse();
+  trueOrFalse.BetweenTrueOrFalse();
+  trueOrFalse.BetweenTrueAndFalse();
 }
---
+++ lib/PowProject.dart
@@ -0,0 +1,12 @@
+//code 9.2
+//lib/PowProject.dart
+class PowProject{
+  void MultiplyByAGivenNumber(int fixedNumber, int givenNumber){
+    int result = fixedNumber * givenNumber;
+    print(result);
+  }
+  void pow(int x, int y){
+    int addition = x + y;
+    print(addition);
+  }
+}
